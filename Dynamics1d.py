1b27897172a84097017531c534c4f2c3ad6aef2e
Scott Pirrie <scott.pirrie@googlemail.com>
2019-07-20 22:12:03 +0100

    Trajectory in time now added
    A secondary graph has been added which shows the height against time.

--- Dynamics1d.py
+++ Dynamics1d.py
@@ -29,22 +29,38 @@ g = -9.8
 # it will reach it's peak at -v0/g
 
 t_max = -v0/g
-
 y_max = 0.5*g*t_max**2 + v0*t_max + y0
-
 y_lim = y_max*1.1
+
+
+# Get the time when the ball hits the ground
+
+x_plus = (-v0 + np.sqrt(v0**2 - 2*g*y0))/g
+x_minus = (-v0 - np.sqrt(v0**2 - 2*g*y0))/g
+
+x_max = np.max([x_plus, x_minus])
+x_lim = x_max*1.1
 
 
 # Run the simulation
 
 # Set up the plot
-fig, ax = plt.subplots()
-ax.set_ylim(-y_max*0.1,y_lim)
+fig, ax = plt.subplots(2,1)
+ax[0].set_ylim(-y_max*0.1,y_lim)
+ax[1].set_ylim(-y_max*0.1,y_lim)
+ax[1].set_xlim(0,x_lim)
 
-# plot the first point
-points = ax.plot(x0, y0, marker='o', linestyle='--')[0]
-time_passed = ax.text(0.01,y_max*0.1,"Time Elapsed (s): 0")
-speed = ax.text(0.01,0, "Velocity (m/s): {0}".format(v0))
+
+# plot the first point in the simulated y axis
+points = ax[0].plot(x0, y0, marker='o', linestyle='--')[0]
+time_passed = ax[0].text(0.01,y_max*0.1,"Time Elapsed (s): 0")
+speed = ax[0].text(0.01,0, "Velocity (m/s): {0}".format(v0))
+
+# plot the first point in the t,y plane to show how position changes with time
+
+ts = [0]
+ys = [y0]
+
 
 for t in np.arange(0,60,0.01):
     
@@ -59,6 +75,13 @@ for t in np.arange(0,60,0.01):
     if y_t >= 0:
         # update the point with the new position
         points.set_data(x0, y_t)
+        
+        #draw the t,y line
+        ts.append(t)
+        ys.append(y_t)
+        line_seg = plt.Line2D(ts, ys)
+        ax[1].add_line(line_seg)
+        
     else:
         # exit the loop if the particle hits the ground at 0
         points.set_data(x0, 0)
